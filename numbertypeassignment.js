f8a808651a60df5e9ccd9b00894a0b41b47223f7
Subhasrisaravanan54 <ssubhasri648@gmail.com>
2026-08-05 23:07:38 +0530

    Add files via upload
    home assignment(number type)-day 3

---
+++ numbertypeassignment.js
@@ -0,0 +1,20 @@
+function demo(number)
+{
+
+    if(number>0)
+    {
+     console.log("given number is positive number");
+     
+    }
+    else if(number<0)
+    {
+     console.log("given number is negative number");
+     
+    }
+    else
+    {
+     console.log("given number is neutal");
+     
+    }
+}
+demo(-1);
