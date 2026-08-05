802e4b3460ac6fabc9b5cc82dfde1b0514851323
Subhasrisaravanan54 <ssubhasri648@gmail.com>
2026-08-05 19:50:05 +0530

    Add files via upload
    Week1 day 2

---
+++ datatype.js
@@ -0,0 +1,27 @@
+
+//String
+var firstname="john";
+console.log('name is',typeof firstname);
+console.log(firstname);
+//string
+var companyname="Agilysys";
+console.log(companyname);
+//number
+var mobilenum=123456789;
+console.log(mobilenum);
+console.log(typeof mobilenum);
+//boolean
+var isautomation=false;
+console.log(isautomation);
+//undefined
+var hasPlaywright
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
+//null
+var lanLine=null;
+console.log(lanLine);
+
+
+
+
+
