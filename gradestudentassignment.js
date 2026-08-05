f84f9b6693a5a624b4da3967d8c9b3923eb0bf0f
Subhasrisaravanan54 <ssubhasri648@gmail.com>
2026-08-05 23:33:58 +0530

    Add files via upload
    Home assignment(student grade)-Week 1 day 3

---
+++ gradestudentassignment.js
@@ -0,0 +1,16 @@
+function student(score)
+{
+    switch(score)
+    {
+        case 1:
+            console.log("first class");
+        case 2:  
+            console.log("second class");
+        default:
+            console.log("Fail");
+             
+               
+            
+    }
+}
+student(35);
