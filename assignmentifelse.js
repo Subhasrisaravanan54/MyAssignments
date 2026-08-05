356a339bdafa9379f291327478379f6efe1d554e
Subhasrisaravanan54 <ssubhasri648@gmail.com>
2026-08-05 20:53:38 +0530

    Add files via upload
    if else program-Day3

---
+++ assignmentifelse.js
@@ -0,0 +1,18 @@
+let browsername="chrome";
+console.log(browsername);
+function launchBrowser()
+{
+if(browsername=="chrome")
+{
+console.log("browsername is chrome");
+
+}
+else
+{
+console.log("browsername is safari");
+
+}
+
+
+}
+launchBrowser();    
